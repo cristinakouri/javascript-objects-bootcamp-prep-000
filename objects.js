83098679eeeb31a509abae5d8bd6b376565e9698
Cristina Kouri <cristina.isabel.kouri@gmail.com>
2018-09-06 19:39:32 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,12 +10,3 @@ function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
   return playlist
 }
-
-function updatePlaylist(playlist, artistName){
-  delete playlist.artistName
-  return playlist
-}
-
-function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName
-}
